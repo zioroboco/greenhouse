5d5a5d8f2cf5d08671f3923f32f0b4baecc10666
Louis Quinnell <_@ziorobo.co>
2022-02-19 01:54:52 +1100

    Copy config from @zioroboco/phyla

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,30 +1,26 @@
 import { createRequire } from "module"
+
 const require = createRequire(import.meta.url)
-const { workspaces } = require("./package.json")
+const { compilerOptions } = require("./tsconfig.json")
 
 /**
  * @type {import("@jest/types").Config.InitialOptions}
  */
-export const common = {
-  moduleFileExtensions: ["ts", "js", "json"],
-  modulePathIgnorePatterns: ["\\.js$"],
+export const config = {
+  testMatch: ["**/*.spec.ts"],
   preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
 
+  resolver: "jest-ts-webcompat-resolver",
+
   globals: {
     "ts-jest": {
+      isolatedModules: true,
       diagnostics: { ignoreCodes: [151001] },
       useESM: true,
-      tsconfig: require("./tsconfig.json").compilerOptions,
+      tsconfig: compilerOptions,
     },
   },
-}
-
-/**
- * @type {import("@jest/types").Config.InitialOptions}
- */
-const config = {
-  projects: workspaces,
 }
 
 export default config
